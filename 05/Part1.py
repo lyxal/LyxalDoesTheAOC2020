e128bcd2ac0a9dddcbe06a4b04926957330d4da8
Lyxal <36217120+Lyxal@users.noreply.github.com>
2020-12-05 16:28:46 +1100

    Create Part1.py

---
+++ 05/Part1.py
@@ -0,0 +1,17 @@
+i = open("input.txt").read().split("\n")
+
+row_up = 128
+row_low = 1
+
+col_up = 8
+col_low = 1
+
+ids = []
+
+for c in i:
+      m = c[0:7].replace("F", '0').replace("B", '1')
+  
+      p = int(m, 2) * 8
+      v = c[7:].replace("R", '1').replace("L", '0')
+      p += int(v, 2)
+      ids.append(p)
